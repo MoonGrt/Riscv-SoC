17a1fb2e29df989cc488bd04599f9fc4f2f7e72d
MoonGrt <1561145394@qq.com>
2024-10-10 20:10:03 +0800

    new

--- tool/HexExtractor.py
+++ tool/HexExtractor.py
@@ -38,27 +38,6 @@ def extract_code(file_path):
 
     return instructions
 
-# def save_bytes_to_files(instructions, output_dir):
-#     """
-#     将每条指令的每个字节保存到四个不同的文件中。
-#     """
-#     file_names = [output_dir, f'{output_dir}0', f'{output_dir}1', f'{output_dir}2', f'{output_dir}3']
-
-#     # 打开文件进行写入
-#     with open(file_names[0], 'w') as f0, \
-#          open(file_names[1], 'w') as f1, \
-#          open(file_names[2], 'w') as f2, \
-#          open(file_names[3], 'w') as f3, \
-#          open(file_names[4], 'w') as f4:
-
-#         for address, instruction in instructions:
-#             # 将每个字节写入相应的文件
-#             f0.write(f'{instruction}\n')  # 全部字节
-#             f1.write(f'{instruction[0:2]}\n')  # 第一个字节
-#             f2.write(f'{instruction[2:4]}\n')  # 第二个字节
-#             f3.write(f'{instruction[4:6]}\n')  # 第三个字节
-#             f4.write(f'{instruction[6:8]}\n')  # 第四个字节
-
 def save_bytes_to_files(instructions, output_dir):
     """
     将每条指令的每个字节保存到四个不同的文件中，确保每个文件写入1024字节数据。
@@ -78,10 +57,6 @@ def save_bytes_to_files(instructions, output_dir):
         bytes_content[2].append(instruction[2:4])  # 第二个字节
         bytes_content[3].append(instruction[4:6])  # 第三个字节
         bytes_content[4].append(instruction[6:8])  # 第四个字节
-        # bytes_content[1].append(instruction[6:8])  # 第一个字节
-        # bytes_content[2].append(instruction[4:6])  # 第二个字节
-        # bytes_content[3].append(instruction[2:4])  # 第三个字节
-        # bytes_content[4].append(instruction[0:2])  # 第四个字节
 
     # 将字节内容写入文件，并确保文件大小为1024字节
     for i in range(5):
