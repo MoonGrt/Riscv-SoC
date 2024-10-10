d1105221f9c94c17e5895e5136187054ef37b779
MoonGrt <1561145394@qq.com>
2024-10-10 21:29:48 +0800

    new

---
+++ code/gpio/src/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <murax.h>
+
+void main()
+{
+    GPIO_A->OUTPUT_ENABLE = 0x000000FF;
+    GPIO_A->OUTPUT = 0x00000011;
+}
+
+void irqCallback()
+{
+
+}
---
+++ code/lib/murax/murax.h
@@ -0,0 +1,22 @@
+#ifndef MURAX_H_
+#define MURAX_H_
+
+#include "timer.h"
+#include "prescaler.h"
+#include "interrupt.h"
+#include "gpio.h"
+#include "uart.h"
+
+#define CORE_HZ 12000000
+
+#define GPIO_A ((Gpio_Reg *)(0xF0000000))
+
+#define TIMER_A ((Timer_Reg *)0xF0020040)
+#define TIMER_B ((Timer_Reg *)0xF0020050)
+#define TIMER_PRESCALER ((Prescaler_Reg *)0xF0020000)
+#define TIMER_INTERRUPT ((InterruptCtrl_Reg *)0xF0020010)
+
+#define UART ((Uart_Reg *)(0xF0010000))
+#define UART_SAMPLE_PER_BAUD 5
+
+#endif /* MURAX_H_ */
